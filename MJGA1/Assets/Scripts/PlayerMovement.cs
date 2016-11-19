1693b712290eb60301e2d6f3c95a05d7e4fe7660
Joshua Watson <josh.m.watson@gmail.com>
2016-11-19 14:27:16 -0600

    added wall and new play physics using 2D Unity

--- MJGA1/Assets/Scripts/PlayerMovement.cs
+++ MJGA1/Assets/Scripts/PlayerMovement.cs
@@ -3,27 +3,27 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    float lrSpeed = 6f;
-    float udSpeed = 8f;
+    public float lrSpeed = 6f;
+    public float udSpeed = 8f;
 
 	// Update is called once per frame
 	void Update () {
 	
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left / lrSpeed);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * lrSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.right / lrSpeed);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * lrSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector2.down / udSpeed);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down * udSpeed);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector2.up / udSpeed);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * udSpeed);
         }
     }
 }
