da7cc616aadcf1a205380e2a77159621ba0eccaf
Stephen Roper <stephenroper@me.com>
2016-11-19 22:59:22 -0600

    Actual bombs should be in this shit now for real.

---
+++ MJGA1/Assets/Scripts/PlayerBombMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBombMovement : MonoBehaviour {
+
+    public float bombSpeed = 5f;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.up * bombSpeed * Time.deltaTime);
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D Enemy)
+    {
+        //ContactPoint contact = collision.contacts[0];
+        //Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        //Vector3 pos = contact.point;
+        //Instantiate(explosionPrefab, pos, rot);
+        if (Enemy.gameObject.tag == "Enemy")
+        {
+            Destroy(Enemy.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    }
