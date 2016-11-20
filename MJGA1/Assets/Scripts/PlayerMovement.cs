ba7f50036a4a9526126888286fbbdbbcb96737ed
Stephen Roper <stephenroper@me.com>
2016-11-19 21:12:16 -0600

    Added Speed Boost

--- MJGA1/Assets/Scripts/PlayerMovement.cs
+++ MJGA1/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,17 @@ public class PlayerMovement : MonoBehaviour {
     public float lrSpeed = 14f;
     public float udSpeed = 11f;
 
-	// Update is called once per frame
-	void Update () {
+    private float lrSpeedBoost = 2;
+    private float udSpeedBoost = 2;
+    private const float lrSpeedMax = 40;
+    private const float udSpeedMax = 35;
+    private float lrSpeedMin = 14f;
+    private float udSpeedMin = 11f;
+
+
+
+    // Update is called once per frame
+    void Update () {
 	
         if (Input.GetKey(KeyCode.A))
         {
@@ -25,5 +34,36 @@ public class PlayerMovement : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * udSpeed);
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            lrSpeed += lrSpeedBoost;
+            udSpeed += udSpeedBoost;
+
+            if (lrSpeed >= lrSpeedMax)
+            {
+                lrSpeed = lrSpeedMax;
+            }
+            if (udSpeed >= udSpeedMax)
+            {
+                udSpeed = udSpeedMax;
+            }
+
+        }
+        else
+        {
+            lrSpeed -= lrSpeedBoost;
+            udSpeed -= udSpeedBoost;
+
+            if (lrSpeed <= lrSpeedMin)
+            {
+                lrSpeed = lrSpeedMin;
+            }
+            if (udSpeed <= udSpeedMin)
+            {
+                udSpeed = udSpeedMin;
+            }
+        }
+
     }
 }
