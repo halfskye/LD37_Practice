24d8bddd6a038585b20062bbd0f792038b726b40
Joshua Watson <josh.m.watson@gmail.com>
2016-11-20 22:42:22 -0600

    added EnemyShip

---
+++ MJGA1/Assets/Scripts/Utilities/GetPath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    class GetPath : MonoBehaviour
+    {
+        public GameObject[] allPaths;
+
+        void Start()
+        {
+            int num = Random.Range(0, allPaths.Length);
+
+            transform.position = allPaths[num].transform.position;
+            MoveOnPath thePath = GetComponent<MoveOnPath>();
+            thePath.pathName = allPaths[num].name;
+        }
+    }
+}
