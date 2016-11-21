0c4faa190b4182320a43fa6fb5eafae0dbec40f7
Stephen Roper <stephenroper@me.com>
2016-11-20 22:12:23 -0600

    Fixed player collisions with asteroids.

--- MJGA1/Assets/Scripts/PlayerMovement.cs
+++ MJGA1/Assets/Scripts/PlayerMovement.cs
@@ -33,7 +33,7 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        Physics2D.IgnoreLayerCollision(8,9);    
+        //Physics2D.IgnoreLayerCollision(8,9);    
 
         #region BasicMovement
         if (Input.GetKey(KeyCode.A))
