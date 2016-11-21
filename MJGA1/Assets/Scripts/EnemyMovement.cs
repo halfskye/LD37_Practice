2106a39fcf46cdf666ad95c238f7be0b7a4ed17d
Stephen Roper <stephenroper@me.com>
2016-11-20 18:21:44 -0600

    Added left and right asteroids

--- MJGA1/Assets/Scripts/EnemyMovement.cs
+++ MJGA1/Assets/Scripts/EnemyMovement.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
-    public float moveSpeed = 1;
+    private float moveSpeed = 1.75f;
     public int rotateSpeed;
     private int x = 0;
     private int y = 0;
 
-	// Use this for initialization
-	void Start () {
+    public enum Type
+    {
+        FALLING_ASTEROID = 0,
+        RIGHT_ASTEROID = 1,
+        LEFT_ASTEROID = 2,
+    };
+
+    [SerializeField]
+    private Type type;
+
+    void Start () {
 
         //Rotate Speed Initialization
         rotateSpeed = Random.Range(-7, 7);
@@ -19,14 +28,24 @@ public class EnemyMovement : MonoBehaviour {
         }
     }
 	
-	// Update is called once per frame
 	void Update () {
         
             //Rotation
             transform.Rotate(0f, 0f, rotateSpeed);
-            
+
+        if (type == Type.FALLING_ASTEROID)
+        {
             //Movement
             GetComponent<Rigidbody2D>().AddForce(Vector2.down * moveSpeed);
-        
+        }
+
+        if (type == Type.RIGHT_ASTEROID)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * moveSpeed + Vector2.left * moveSpeed);
+        }
+        if (type == Type.LEFT_ASTEROID)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * moveSpeed + Vector2.right * moveSpeed);
+        }
     }
 }
--- MJGA1/Assets/Scripts/EnemySpawner.cs
+++ MJGA1/Assets/Scripts/EnemySpawner.cs
@@ -6,13 +6,17 @@ public class EnemySpawner : MonoBehaviour {
     private float Timer;
 
     public Vector3 enemySpawn;
+    public Vector3 enemyDiagRightSpawn;
+    public Vector3 enemyDiagLeftSpawn;
     float x;
-    float y = 6;
+    float y;
     float z = 0;
 
 
 
     GameObject enemy;
+    GameObject enemyDiagRight;
+    GameObject enemyDiagLeft;
 
 
 	// Use this for initialization
@@ -23,18 +27,44 @@ public class EnemySpawner : MonoBehaviour {
 
 	void Update () {
 
-        int spawnTimer = Random.Range(1, 70);
+        int spawnTimer = Random.Range(1, 700);
 
-        if (spawnTimer == 3)
+        //int spawnTimer = 1;
+
+        if (spawnTimer == 4 || spawnTimer == 5 || spawnTimer == 6 || spawnTimer == 7)
             {
                 
                 x = Random.Range(-8f, 7.5f);
+                y = 6;
                 enemy = (GameObject)Instantiate(Resources.Load("Enemy"));
                                 
                 enemySpawn = new Vector3(x, y, z);
                 
                 enemy.transform.position = enemySpawn;
-               
+        }
+
+        if (spawnTimer == 1)
+        {
+            x = Random.Range (11, 14);
+            y = -7;
+
+            enemyDiagRight = (GameObject)Instantiate(Resources.Load("DiagAsteroidRight"));
+            enemyDiagRightSpawn = new Vector3(x, y, z);
+
+            enemyDiagRight.transform.position = enemyDiagRightSpawn;
+
+        }
+
+        if (spawnTimer == 2)
+        {
+            x = Random.Range(-13, -10);
+            y = -7;
+
+            enemyDiagLeft = (GameObject)Instantiate(Resources.Load("DiagAsteroidLeft"));
+            enemyDiagLeftSpawn = new Vector3(x, y, z);
+
+            enemyDiagLeft.transform.position = enemyDiagLeftSpawn;
+
         }
 
 
